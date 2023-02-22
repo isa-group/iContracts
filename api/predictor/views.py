c006e524871416f5d3f22391f9bb0034d98b580c
marmolpen3 <marmolpen3@alum.us.es>
2023-02-23 00:17:04 +0100

    feat: Add decoder to split de text in sentences

--- api/predictor/views.py
+++ api/predictor/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, g, Blueprint
+from flask import g, Blueprint
 from .paragraph_decoder import paragraph_decoder
 
 predictor = Blueprint('predictor', __name__)
@@ -6,7 +6,5 @@ predictor = Blueprint('predictor', __name__)
 @predictor.post('/prediction')
 @paragraph_decoder
 def predict():
-    paragraph = request.json['paragraph']
-    print(paragraph)
-    prediction = g.text
+    prediction = g.sent_list
     return f'{prediction}'
