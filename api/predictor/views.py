90d8f172af995b974cfb825b4032d05b27f37588
marmolpen3 <marmolpen3@alum.us.es>
2023-02-23 02:26:22 +0100

    feat: Update response format

--- api/predictor/views.py
+++ api/predictor/views.py
@@ -4,10 +4,16 @@ from setfit import SetFitModel
 
 predictor = Blueprint('predictor', __name__)
 
+
 @predictor.post('/prediction')
 @paragraph_decoder
 def predict():
     sentences_list = g.sent_list
-    model = SetFitModel.from_pretrained("marmolpen3/p-MiniLM-L3-v2-sla-obligations-rights")
+    sentences_list = [s.replace('"', '') for s in sentences_list]
+    model = SetFitModel.from_pretrained(
+        "marmolpen3/p-MiniLM-L3-v2-sla-obligations-rights")
     predictions = model(sentences_list)
-    return f'{predictions}'
+    labels = ["Obligations", "Rights", "Neither"]
+    label_results = [labels[p] for p in predictions]
+    result = dict(zip(sentences_list, label_results))
+    return result
