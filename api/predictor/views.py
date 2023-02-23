13777743d64619c4bab7c0f0d00aac9ce2e5478e
marmolpen3 <marmolpen3@alum.us.es>
2023-02-23 01:38:36 +0100

    feat: Add predictor model

--- api/predictor/views.py
+++ api/predictor/views.py
@@ -1,10 +1,13 @@
 from flask import g, Blueprint
 from .paragraph_decoder import paragraph_decoder
+from setfit import SetFitModel
 
 predictor = Blueprint('predictor', __name__)
 
 @predictor.post('/prediction')
 @paragraph_decoder
 def predict():
-    prediction = g.sent_list
-    return f'{prediction}'
+    sentences_list = g.sent_list
+    model = SetFitModel.from_pretrained("marmolpen3/p-MiniLM-L3-v2-sla-obligations-rights")
+    predictions = model(sentences_list)
+    return f'{predictions}'
