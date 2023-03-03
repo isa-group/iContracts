2be33835a6e108510c0337b060b09f215535399f
marmolpen3 <marmolpen3@alum.us.es>
2023-03-03 16:47:13 +0100

    Change model

--- flask/api/predictor/views.py
+++ flask/api/predictor/views.py
@@ -4,14 +4,14 @@ from setfit import SetFitModel
 
 predictor = Blueprint('predictor', __name__)
 
+model = SetFitModel.from_pretrained("marmolpen3/p-MiniLM-L3-v2-sla-obligations-rights")
 
-@predictor.post('/api/v1/predictions')
+@predictor.post('/predictions')
 @paragraph_decoder
 def predict():
+    print("Hola")
     sentences_list = g.sent_list
     sentences_list = [s.replace('"', '') for s in sentences_list]
-    model = SetFitModel.from_pretrained(
-        "marmolpen3/p-MiniLM-L3-v2-sla-obligations-rights")
     predictions = model(sentences_list)
     labels = ["Obligations", "Rights", "Neither"]
     label_results = [labels[p] for p in predictions]
